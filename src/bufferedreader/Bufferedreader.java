efbcac7cbd02f89f155879cafcd9acc438d59559
Gabriel Fernandes <kaiberimanos3@gmail.com>
2023-03-12 15:35:38 -0300

    Adicionando o bloco try-with-resources

--- src/bufferedreader/Bufferedreader.java
+++ src/bufferedreader/Bufferedreader.java
@@ -21,22 +21,24 @@ public class Bufferedreader {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BufferedReader zi = null;
-        try {
-            zi = new BufferedReader(new FileReader("C:\\Games\\pi.txt"));
-            String path = zi.readLine();
-            while(path != null){
-                System.out.println(path);
-                path = zi.readLine();
+        /*
+        utilizando o bloco try-with-resources o neste caso especifico o comando zi.close() que faria o fechamento do leitor de texto será executado 
+        automaticamente ao final da execução do bloco try
+        
+        Apenas lembrando: 
+        
+        BufferedReader nomeDoObjeto = new BufferedReader(new FileReader (String com o caminho do arquivo txt que você deseja ler))
+        */
+        try (BufferedReader zi = new BufferedReader(new FileReader("C:\\Games\\pi.txt"))){
+            String line = zi.readLine();
+            while(line != null){
+                System.out.println(line);
+                line = zi.readLine();
             }
-            zi.close();
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
         }catch(IOException e){
             System.out.println(e.getMessage());
-        }finally{
-            String response = zi == null?"Impossivel inicializar":"Sucesso ao ler arquivo";
-            System.out.println(response);
         }
            
         
